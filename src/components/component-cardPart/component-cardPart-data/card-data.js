dcebd5a5638bde8a6af02872fd348a9a93318afe
Abhishek <abhiofficial581@gmail.com>
2022-10-28 16:41:06 +0530

    Card UI change and images

--- src/components/component-cardPart/component-cardPart-data/card-data.js
+++ src/components/component-cardPart/component-cardPart-data/card-data.js
@@ -1,26 +1,37 @@
-import profilePic from '../../../images/profilePic.jpg'
+import Lawliet from '../../../images/profilePic.jpg'
 import kakashi from '../../../images/kakashi2.jpg'
 import hachiman from '../../../images/hachiman.png'
 import oregi from '../../../images/oregi1.jpg'
-import ayanokoji from '../../../images/wp3666334-ayanokoji-wallpapers.png'
+import ayanokoji from '../../../images/wp3666334-ayanokoji-wallpapers.jpg'
 import itachi from '../../../images/wallpaperflare.com_wallpaper.jpg'
-
+import gow1 from '../../../images/gow1.jpg'
+import gow2 from '../../../images/gow2.jpg'
 
 const Carddata=[
     {
         id:0,
         firstName:"Lawliet",
         lastName:"laruso",
-        profilePic: profilePic,
+        profilePic: Lawliet,
+        banner:gow1,
         active:true,
-        description:"Best Detective in world."
+        from: "Delhi",
+        country:"India",
+        college:"IIIT Delhi",
+        specialization:"Web Dev",
+        description:"Best Detective in world. Also Known as L in death Note.Back Problems"
     },
     {
         id:1,
         firstName:"kakashi",
         lastName:"hatake",
         profilePic: kakashi,
+        banner:gow2,
         active:false,
+        from: "Mumbai",
+        country:"India",
+        college:"IIT Bombay",
+        specialization:"Compeitive Programming",
         description:"6th Hokage and sensei of 7th hockage."
     },
     {
@@ -28,7 +39,12 @@ const Carddata=[
         firstName:"Hachiman",
         lastName:"Hikigya",
         profilePic: hachiman,
+        banner:gow1,
         active:false,
+        from: "Chandighar",
+        country:"India",
+        college:"Lovely University",
+        specialization:"Arts",
         description:"High schooler with good observation."
     },
     {
@@ -36,7 +52,12 @@ const Carddata=[
         firstName:"Oreki",
         lastName:"san",
         profilePic: oregi,
+        banner:gow2,
         active:false,
+        from: "Sri City",
+        country:"India",
+        college:"IIIT Sri City",
+        specialization:"App Dev",
         description:"High schooler, good in theories."
     },
     {
@@ -44,7 +65,12 @@ const Carddata=[
         firstName:"Kiyotaka",
         lastName:"Ayanokoji",
         profilePic: ayanokoji,
+        banner:gow1,
         active:false,
+        from: "Chennai",
+        country:"India",
+        college:"Kalra University",
+        specialization:"Web Dev",
         description:"Topper of White Room."
     },
     {
@@ -52,7 +78,12 @@ const Carddata=[
         firstName:"Itachi",
         lastName:"Uchiha",
         profilePic: itachi,
+        banner:gow2,
         active:false,
+        from: "Jaipur",
+        country:"India",
+        college:"Babusa University",
+        specialization:"English",
         description:"Akatsuki Member and best in Gengutsu."
     },
 ]
--- src/components/component-cardPart/component-cardPart-src/Card.js
+++ src/components/component-cardPart/component-cardPart-src/Card.js
@@ -1,18 +1,30 @@
 import '../component-cardPart-css/Card.css'
+import SchoolIcon from '@mui/icons-material/School';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Carddata from '../component-cardPart-data/card-data'
 const Card=(props)=>{
     // console.log(props.id);
     return(
         <div class="card">
-            <img class="card-img-top" src={Carddata[props.id].profilePic} alt="Card image cap"></img>
-            <div id='card-body'>
-                <div id='nameDiv'>
-                    <h1>{Carddata[props.id].firstName} {Carddata[props.id].lastName}</h1>
-                </div>
-                <div id="description">
-                    <h4>{Carddata[props.id].description}</h4>
-                </div>
+            <img class="card-img-banner" src={Carddata[props.id].banner} alt="Card image cap"></img>
+           <a href='#'><img className='card-img-profilpic' src={Carddata[props.id].profilePic} alt="profilPic"></img></a>
+           <div id='name-div'>
+                <h1>{Carddata[props.id].firstName} {Carddata[props.id].lastName}</h1>
             </div>
+            <div id='div-city'>
+                <h4><LocationOnIcon/> {Carddata[props.id].from} , {Carddata[props.id].country}</h4>
+            </div>
+            <div id='div-college'>
+                <h4><SchoolIcon/> {Carddata[props.id].college}</h4>
+            </div>
+            <div id='div-pecialization'>
+                <h4><LibraryBooksIcon/> {Carddata[props.id].specialization}</h4>
+            </div>
+            <div id='div-description'>
+                <h5>{Carddata[props.id].description}</h5>
+            </div>
+
         </div>
     )
 }
