dada13c46c28236ac0b90576b85159d0e6e7ac56
Abhishek <abhiofficial581@gmail.com>
2022-10-28 10:14:14 +0530

    card Swipe

--- src/components/component-cardPart/component-cardPart-src/CardPart.js
+++ src/components/component-cardPart/component-cardPart-src/CardPart.js
@@ -5,58 +5,67 @@ import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import Carddata from '../component-cardPart-data/card-data'
 import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
 import data from '../../component-main-page/component-main-page-data/data'
+import TinderCard from 'react-tinder-card'
+import { ReactDOM } from 'react';
 const CardPart=()=>{
-    
     let dataActive=[]
     Carddata.map((person)=>{
         dataActive.push(person.active)
     })
-    // console.log(dataActive);
-    // let dataActive=[true,false]
-    const [active,setActive]=useState(dataActive)
-    // console.log(active);
-    const swipeEffect=()=>{
-        let ele=document.getElementsByClassName('cards')[0]
-        ele.classList.add('SwipeleftEffect')
-    }
-    const handleClick=()=>{
-        swipeEffect()
-        let array=[];
-        console.log(active);
-        for(let i=0;i<active.length;i++){
-            array.push(active[i]);
+    let [active,setActive]=useState(dataActive)
+    const characters = Carddata
+    const [lastDirection, setLastDirection] = useState()
+    
+    const swiped = (direction, nameToDelete) => {
+        console.log(direction);
+        if(direction=='right'){
+            document.getElementsByClassName('cardPart')[0].classList.add('bgGreen')
         }
-        for(let i=0;i<array.length;i++){
-            if(array[i]==true){
-                array[i]=false;
-                array[(i+1)%array.length]=true;
-                break;
-            }
+        else{
+            document.getElementsByClassName('cardPart')[0].classList.add('bgRed')
         }
-        setActive(array)
-        console.log(active);
-        
-        // return array
-    }
+        console.log('removing: ' + nameToDelete)
+        setLastDirection(direction)
+  }
 
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+    console.log(active);
+    document.getElementsByClassName('cardPart')[0].classList.remove('bgGreen')
+    document.getElementsByClassName('cardPart')[0].classList.remove('bgRed')
+    let array=[];
+    for(let i=0;i<active.length;i++){
+        array.push(active[i]);
+    }
+    for(let i=0;i<array.length;i++){
+        if(array[i]==true){
+            array[i]=false;
+            array[(i+1)%array.length]=true;
+            break;
+        }
+    }
+    console.log(array);
+    active=array
+    setActive(active)
+    console.log(active);
+
+  }
     return(
-        <div className="cardPart col-6">
-            {
-                Carddata.map((person)=>{
-                    console.log(person.id);
-                    return(
-                        <div className={active[person.id]? 'cards': 'remove-card'}>
-                            <Card id={person.id}/>
-                        </div>
-                    )
-                })
-            }
-            <div id='buttonsDiv'>
-                <button id='acceptBtn' onClick={handleClick}><ThumbUpOffAltIcon/></button>
+    <div className="cardPart col-6">
+      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+        {characters.map((character) =>
+          <TinderCard className={active[character.id]? 'swipe':'remove-card'} key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+            <Card id={character.id}/>
+          </TinderCard>
+        )}
+            {/* {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />} */}
+            {/* <div id='buttonsDiv'>
+                <button id='acceptBtn' ><ThumbUpOffAltIcon/></button>
                 <br></br>
-                <button id='rejectBtn' onClick={handleClick}><ThumbDownOffAltIcon/></button>
-            </div>
-        </div>
+                <button id='rejectBtn'><ThumbDownOffAltIcon/></button>
+            </div> */}
+    </div>
     )
 }
 export default CardPart
